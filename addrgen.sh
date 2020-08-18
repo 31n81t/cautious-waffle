3bad8237ff010e0c181e2813c78286570c25df87
Maigurs <31n81t@gmx.com>
2020-08-18 20:39:40 +0100

    Update addrgen.sh

--- addrgen.sh
+++ addrgen.sh
@@ -3,7 +3,6 @@ bitcoin-cli createwallet "wallet`date +%d%H%M%S`" > tmp.txt && \
 tr -d  '[:punct:]' < tmp.txt > tmp1.txt && \
 awk '{ print $2 }' tmp1.txt > tmp.txt && \
 bitcoin-cli -rpcwallet="$(< tmp.txt)" getnewaddress `cat tmp.txt` > tmp2.txt && \
-bitcoin-cli -rpcwallet="$(< tmp.txt)" getbalance > tmp3.txt && \
 bitcoin-cli -rpcwallet="$(< tmp.txt)" dumpprivkey `cat tmp2.txt` >> tmp3.txt && \
 cat tmp2.txt >> tmp3.txt && \
 cat tmp3.txt && \
