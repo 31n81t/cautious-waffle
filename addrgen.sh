965e726c2c9ac6a2c09e1116411c801853836e28
Maigurs <31n81t@gmx.com>
2020-09-07 18:11:47 +0100

    Update addrgen.sh
    Compacting code-base by removing unnecessary characters and symbols.

--- addrgen.sh
+++ addrgen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-bitcoin-cli createwallet "wallet`date +%d%H%M%S`" > tmp.txt && \
-tr -d  '[:punct:]' < tmp.txt > tmp1.txt && \
-awk '{ print $2 }' tmp1.txt > tmp.txt && \
-bitcoin-cli -rpcwallet="$(< tmp.txt)" getnewaddress `cat tmp.txt` > tmp2.txt && \
-bitcoin-cli -rpcwallet="$(< tmp.txt)" dumpprivkey `cat tmp2.txt` >> tmp3.txt && \
-cat tmp2.txt >> tmp3.txt && \
-cat tmp3.txt && \
-mv tmp3.txt wallet`date +%d%H%M%S`.txt && \
-rm tmp* && \
+bitcoin-cli createwallet "wallet`date +%d%H%M%S`" > tmp.txt
+tr -d  '[:punct:]' < tmp.txt > tmp1.txt
+awk '{ print $2 }' tmp1.txt > tmp.txt
+bitcoin-cli -rpcwallet="$(< tmp.txt)" getnewaddress `cat tmp.txt` > tmp2.txt
+bitcoin-cli -rpcwallet="$(< tmp.txt)" dumpprivkey `cat tmp2.txt` >> tmp3.txt
+cat tmp2.txt >> tmp3.txt
+cat tmp3.txt
+mv tmp3.txt wallet`date +%d%H%M%S`.txt
+rm tmp*
 exit
